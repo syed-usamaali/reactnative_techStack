703dbf332d1874ddfd89af10737a71e121a198f9
usama-aoitechs <61180500+usama-aoitechs@users.noreply.github.com>
2020-02-24 13:30:42 +0500

    Moved to src

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Tech Stack</Text>
+    </View>
+  );
+};
+
+export default App;
